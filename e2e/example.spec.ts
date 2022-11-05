d3129725221155b856ca7d27e94b98429c6805d5
Michael Helvey <michael@michaelhelvey.dev>
2022-11-05 15:36:10 -0500

    Integrated playwright

---
+++ e2e/example.spec.ts
@@ -0,0 +1,12 @@
+import { expect, test } from '@playwright/test'
+
+test('homepage has title and links to intro page', async ({ page }) => {
+	await page.goto('/')
+
+	// Expect a title "to contain" a substring.
+	await expect(page).toHaveTitle(/Next.js Starter Project/)
+
+	await expect(page.getByRole('heading')).toHaveText(
+		'Next.js 13 Starter Project'
+	)
+})
